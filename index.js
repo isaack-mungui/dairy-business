7f09a6588ed00314dbc13032fbdcf5e3e90b1ad9
isaack-mungui <munguiisaack@gmail.com>
2022-07-16 19:15:10 +0300

    Added function to add input field on selection from dropdown

---
+++ index.js
@@ -0,0 +1,14 @@
+function addInputField(elem) {
+    if (elem.selectedIndex != 0) {
+        document.getElementById("input-field").style.display = 'block';
+    } else {
+        document.getElementById("input-field").style.display = 'none';
+    }
+}
+
+// const totalProduction = (shedA, shedB, shedC, shedD) => {
+//     let production = document.getElementById("value-field");
+//     for (let i = 0; i < production.length; i++) {
+//         document.write(`Your production in Shed A: ${}`)
+//     }
+// }
